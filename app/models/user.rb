daa8ecc65079585496b003134557c092ea2bbb68
Emily Leathers <peppermintdove@gmail.com>
2012-11-18 13:30:18 -0800

    also need User's active attribute to be mass-assignable so that User edit can be used

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   has_many :stars_given, :class_name => 'Star', :foreign_key => :from_id
   
-  attr_accessible :name, :email, :password, :password_confirmation, :facebook_uid
+  attr_accessible :name, :email, :password, :password_confirmation, :facebook_uid, :active
 
   ###
   # Named Scopes
