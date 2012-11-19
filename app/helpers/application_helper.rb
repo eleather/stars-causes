de883034a4ab64d2751830ef002b96a5813b4a6c
Emily Leathers <peppermintdove@gmail.com>
2012-11-19 14:28:56 -0800

    factor out code to get user's avatar so it can be used by both photo and background_photo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,35 +14,43 @@ module ApplicationHelper
          sub('AM', 'am').
          sub('PM', 'pm')
   end
-
-  def photo(user)
+  
+  def user_photo_url(user, type='square')
     if user.facebook_uid
       fb_uid = user.facebook_uid.to_i
-      image_url = "http://graph.facebook.com/#{fb_uid}/picture?type=square"
-      image_tag = image_tag(
-        image_url, 
-        :class => 'user_photo', 
-        :alt => '',
-        :title => user.name
-      )
+      image_url = "http://graph.facebook.com/#{fb_uid}/picture?type=#{type}"
     else
-      image_tag = image_tag(
-        'user.png',
-        :class => 'user_photo', 
-        :alt => '',
-        :title => user.name
-      )
+      image_url = 'user.png'
     end
+    
+    return image_url
+  end
+
+  def photo(user)
+    image_url = user_photo_url(user, 'square')
+    image_tag = image_tag(
+      image_url, 
+      :class => 'user_photo', 
+      :alt => '',
+      :title => user.name
+    )
       
     link_to image_tag, user_url(user)
   end
 
   def background_photo(user)
-    image_url =
-      "http://graph.facebook.com/#{user.facebook_uid.to_i}/picture?type=large"
-    div = content_tag(:div, '', :class => 'user_photo', :title => user.name,
-                      :style => "background-image: url('#{image_url}');" +
-                        'background-position: center; background-size: cover')
+    image_url = user_photo_url(user, 'large')
+    div = content_tag(
+      :div, 
+      '', 
+      {
+        :class => 'user_photo', 
+        :title => user.name,
+        :style => "background-image: url('#{image_url}');" +
+                  'background-position: center; background-size: cover'
+      }
+    )
+
     link_to div, user_path(user)
   end
 end
