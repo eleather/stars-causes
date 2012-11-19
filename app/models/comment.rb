5bb0c67e30301df2e2a87e53cda17615b79e0f52
Emily Leathers <peppermintdove@gmail.com>
2012-11-19 12:50:50 -0800

    update Star model and star partial comment form for rails3 compatibility (attr_attribute for mass-settable attributes, form error messages)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, :class_name => 'User'
   belongs_to :star
+  
   validates_presence_of :author, :body, :star
+  
+  attr_accessible :star_id, :author_id
   
   after_create do |comment|
     Mailer.deliver_comment(comment)
